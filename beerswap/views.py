8dfe8b98f46228252b35189dec9950a1764d0bb1
Locker537 <locker537@gmail.com>
2012-04-16 13:52:01 -0400

    This is ghetto place to add titles to people, but it works for now and is entertaining

--- beerswap/views.py
+++ beerswap/views.py
@@ -33,9 +33,34 @@ def person_detail(request, person_id):
     return render_to_response('beerswap/person.html', {'person': p, 'beers': beers})
 
 def leaderboard(request):
+    meta = {'first':'Grand Brewmeister',
+              'second':'King of the Growlers',
+              'third':'Prince of the Pint',
+              'middle':'Common pint drinker',
+              'last':'Should have brought Natty Light'
+    }
+    
     people = Person.objects.all()
     averages = {}
-    
+
+    # This is ghetto ;-)
+    titles = []
+    for i in range(len(people)):
+        if i == 0:
+            titles.append(meta['first'])
+
+        elif i == 1:
+            titles.append(meta['second'])
+
+        elif i == 2:
+            titles.append(meta['third'])
+
+        elif i == (len(people) - 1):
+            titles.append(meta['last'])
+
+        else:
+            titles.append(meta['middle'])
+            
     for p in people:
         beers = Beer.objects.filter(person__id = p.id)
 
@@ -48,5 +73,12 @@ def leaderboard(request):
         averages[p] = avg
    
     sortedAverages = sorted(averages.iteritems(), key=operator.itemgetter(1), reverse=True)
-    return render_to_response('beerswap/leaderboard.html', {'averages': sortedAverages})
+
+    # This is ghetto ;-)
+    finalData = []
+    for i, each in enumerate(sortedAverages):
+        x = (each[0], (each[1], titles[i]))
+        finalData.append(x)
+        
+    return render_to_response('beerswap/leaderboard.html', {'averages': finalData})
     
